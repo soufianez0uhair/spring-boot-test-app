9bf95eb6412eb4c642b797f89e143bbb5bfb0b58
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-09-22 16:49:23 +0100

    Added settings for database connection in application.properties

---
+++ src/main/java/com/example/springfullproject2/Springfullproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.springfullproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springfullproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springfullproject2Application.class, args);
+	}
+
+}
